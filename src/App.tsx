e22350d3fceb4dcd4722eb608fac4c94edcfb825
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-10-15 06:49:43 +0300

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createPages } from './utils/creatPages';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+type Photo = {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
+
+export const App = () => {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pages, setPages] = useState<number[]>([]);
+  const pagesCount = Math.ceil(totalCount / 10);
+
+  useEffect(() => {
+    axios
+      .get<Photo[]>(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`)
+      .then((response) => {
+        setPhotos([...response.data]);
+        setTotalCount(Number(response.headers['x-total-count']));
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  useEffect(() => {
+    setPages(createPages([], pagesCount, currentPage));
+  }, [currentPage, pagesCount, totalCount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {photos.map((photo) => (
+        <div className='photo'>
+          <div className='title'>
+            {photo.id}. {photo.title}
+            <img
+              src={photo.thumbnailUrl}
+              alt=''
+              width={150}
+              height={150}
+              decoding='async'
+              loading='lazy'
+              className='img'
+            />
+          </div>
+        </div>
+      ))}
+
+      <div className='pages'>
+        {pages?.map((page, index) => (
+          <span
+            key={index}
+            className={currentPage === page ? 'current-page' : 'page'}
+            onClick={() => setCurrentPage(page)}
+          >
+            {page}
+          </span>
+        ))}
+      </div>
     </div>
   );
-}
-
-export default App;
+};
